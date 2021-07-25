f2ee9d5f0e86f3ad83ad0a63680fc4051a7ad553
Nargiza <n.kasymova2021@gmail.com>
2021-07-25 23:18:56 +0600

    переключатель текста

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,9 +5,14 @@ const $headerButton = document.querySelector('.header-button')
 
 
 $headerButton.addEventListener('click', (event) => {
-    console.log(event.target)
-    $page.classList.toggle('night-version');
-    $button.classList.toggle('night-version');
-    $headerButton.textContent = "Ночная версия сайта";
+    if($headerButton.textContent === "Ночная версия сайта"){
+        $page.classList.toggle('night-version');
+        $button.classList.toggle('night-version');
+        $headerButton.textContent = "Дневная версия сайта"
+    } else {
+        $page.classList.toggle('night-version');
+        $button.classList.toggle('night-version');
+        $headerButton.textContent = "Ночная версия сайта"
+    }
 })
 

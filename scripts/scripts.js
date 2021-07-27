ed8a1e86b35b53df937ef57ca3adab3e5d234a62
Nargiza <n.kasymova2021@gmail.com>
2021-07-27 19:10:43 +0600

    body added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,20 @@
 const $page = document.querySelector(".page")
 const $button = document.querySelector('button')
 
+
 const $headerButton = document.querySelector('.header-button')
 
 
 $headerButton.addEventListener('click', (event) => {
     if($headerButton.textContent === "Ночная версия сайта"){
-        $page.classList.toggle('night-version');
-        $button.classList.toggle('night-version');
+        // $page.classList.toggle('night-version');
+        // $button.classList.toggle('night-version');
+        document.body.classList.toggle('night-version');
         $headerButton.textContent = "Дневная версия сайта"
     } else {
-        $page.classList.toggle('night-version');
-        $button.classList.toggle('night-version');
+        // $page.classList.toggle('night-version');
+        // $button.classList.toggle('night-version');
+        document.body.classList.toggle('night-version');
         $headerButton.textContent = "Ночная версия сайта"
     }
 })
